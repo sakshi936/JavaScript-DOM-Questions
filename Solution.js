d466cb3e1cf1e4ee1ba76477a1f65089ddb737c2
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-04-22 00:40:16 +0530

    feat: js-dom-practice-questions

---
+++ Solution.js
@@ -0,0 +1,9 @@
+// Question 1
+
+document.querySelector('.crayons-subtitle-2').innerHTML = `SAKSHI PANDEY`;
+document.querySelector('.color-base-70').innerHTML = 'Exited to learn new things'
+
+// Question 2
+
+
+
