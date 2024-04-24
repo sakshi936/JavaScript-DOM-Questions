a2cbd902e7fbd7f16af0c91a2124ce4cb7d83ebd
Sakshi Pandey <130241239+sakshi936@users.noreply.github.com>
2024-04-24 14:49:42 +0530

    Solution.js

--- Solution.js
+++ Solution.js
@@ -1,6 +1,6 @@
 // Question 1
 
-document.querySelector('.crayons-subtitle-2').innerHTML = `SAKSHI PANDEY`;
+document.querySelector('.crayons-subtitle-2').innerHTML = `MY NAME`;
 document.querySelector('.color-base-70').innerHTML = 'Exited to learn new things'
 
 // Question 2
