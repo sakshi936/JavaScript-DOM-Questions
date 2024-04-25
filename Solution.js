e9aeeaaa673476907270c70bc2d4cbf46d5d2f4d
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-04-26 00:44:43 +0530

    feat: more-solutions

--- Solution.js
+++ Solution.js
@@ -7,3 +7,14 @@ document.querySelector('.color-base-70').innerHTML = 'Exited to learn new things
 
 
 
+// Question 3
+
+const newsec= document.createElement('section');
+newsec.setAttribute('class','parent');
+const h3 = document.createElement('h3');
+let text = document.createTextNode(`My New FAQ`);
+h3.appendChild(text);
+newsec.appendChild(h3);
+const nav = document.querySelector('.accordion-homepage');
+nav.appendChild(newsec);
+
