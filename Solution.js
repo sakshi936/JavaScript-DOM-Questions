fc5f1d0d22ab26b52534ea548470e8f39e8d639b
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-04-30 18:40:35 +0530

    feat: js-dom-practice-questions

--- Solution.js
+++ Solution.js
@@ -34,4 +34,92 @@ const search= document.querySelector('._header_bottom_558sn_1 ._auxiliary_menu_5
 search.addEventListener('mouseover',()=>{search. style.backgroundColor='red'});
 
 
+//Question 7
+
+const formSearch = document.querySelector('.top-navigation-main .header-search form input');
+const searchText ='CSS:Selector';
+formSearch.value= searchText;
+const submit = document.querySelector('.top-navigation-main .header-search form .search-button');
+submit.click();
+
+// Question 8
+
+const lang = document.querySelector('#SIvCob .z4hgWe');
+for (let i = lang.children.length - 1; i >= 0; i -= 2){
+
+    lang.removeChild(lang.children[i]);
+
+
+}
+
+//  Question 9
+
+const heading = document.querySelector('header .text-align-center h1');
+heading.style.fontFamily= 'monospace';
+heading.style.color= '#ff4d4d';
+
+//  Question 10
+
+const startbutton = document.querySelector('main .w-full .signup-btn .login-btn-text');
+
+startbutton.addEventListener(`mouseover`, function (){
+    startbutton.style.backgroundColor='red';
+} );
+
+startbutton.addEventListener(`mouseout`, function (){
+    startbutton.style.backgroundColor='';
+} );
+
+
+
+//  Question 11
+
+// 	https://neurolab.ineuron.ai/images/ineuron-logo-white.png
+
+const logo = document.querySelector(' header .wrapper a');
+logo.removeChild(logo.children[0]);
+let img = document.createElement('img');
+img.src='https://neurolab.ineuron.ai/images/ineuron-logo-white.png';
+logo.appendChild(img);
+
+
+//  Question 12
+
+const button = document.querySelector('.FormControl-spacingWrapper button'); 
+button.style.backgroundColor='blue';
+
+// Question 14
+const HotDeals = document.querySelector('.HotDealsAll__HotCampaignsEventsContainer__FK0V2 .HotDealsAll__Heading__2fIbe');
+HotDeals.style.fontSize ='80px';
+
+
+// Question 15
+
+const pctitle = document.querySelector('#gn5530d83m6001orb1 .ps-top .ps-system-title-container h3');
+pctitle.style.textAlign='right';
+
+// Question 16
+
+// Question 17
+const btn = document.querySelector('.ProductSummaryModels__Content .ProductSummaryModels__ButtonContainer a');
+btn.setAttribute('style',"background-color: transparent; color: black;");
+btn.innerText= new Date();
+
+// Question 18
+
+const footer =document.querySelector('.footerpage .p-f03v2__container .p-f03v2__bottom-section');
+footer.style.backgroundColor='#FEBE10';
+
+// Question 19
+
+let src =document.querySelector('.navbar-header a img').src;
+src;
+
+
+
+// Question 20
+
+
+
+
 
